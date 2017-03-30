0301661a4d34b22a12c3389cce8b73f750c0fe0e
Dustin Augstein <dustin.augstein@match2blue.com>
2017-03-30 08:35:42 +0200

    added inline documentation

--- demo/app/src/main/java/de/stroeer/demoapp/MyActivity.java
+++ demo/app/src/main/java/de/stroeer/demoapp/MyActivity.java
@@ -19,66 +19,133 @@ public class MyActivity extends Activity implements Gateway.IGatewayListener {
 
     public static final String API_KEY = "Type Api-Key here";
 
+    // A simple TextView which will show all given messages to the user
     private TextView debugView;
 
+    /*
+     * onCreate-Method of the Activity
+     *
+     * see Android-Documentation for more information:
+     * https://developer.android.com/reference/android/app/Activity.html
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //calls the onCreate-Method of the super class "Activity"
         super.onCreate(savedInstanceState);
+
+        //Sets the layout for this activity
         setContentView(R.layout.activity_my);
+
+        //returns the instance of the TextView
         this.debugView = (TextView) findViewById(R.id.debugView);
 
         //Defines the ApiKey used by the sdk
         StroeerProxityApi.getInstance(this).setApiKey(API_KEY);
 
-        //Define a custom advertisingID
+        //Defines a custom advertisingID
         StroeerProxityApi.getInstance(this).setCustomAdvertisingId("Type Custom Advertising Id here");
 
         //The Sdk will add the google advertisingId to each Analytics Event
         StroeerProxityApi.getInstance(this).addSystemAdvertisingId(true);
 
-        //Set Path for log-file
+        //Get default external storage path from system
         String path = Environment.getExternalStorageDirectory().getPath() + "/";
+
+        //Set Path of the log-file
         StroeerProxityApi.getInstance(this).setLogFile(new File(path + "logfile.txt"));
 
+        //Start scan for beacons
         StroeerProxityApi.getInstance(this).startScan();
     }
 
+    /*
+     * onResume-Method of the Activity
+     *
+     * see Android-Documentation for more information:
+     * https://developer.android.com/reference/android/app/Activity.html
+     */
     @Override
     protected void onResume() {
+        //calls the onResume-Method of the super class "Activity"
         super.onResume();
+
+        //register the GatewayListener which will get all information sent by the sdk
         StroeerProxityApi.getInstance(this).registerGatewayListener(this);
+
+        //get all states of the sdk again (synchronize app and sdk)
         StroeerProxityApi.getInstance(this).resendCurrentState();
     }
 
+    /*
+     * onPause-Method of the Activity
+     *
+     * see Android-Documentation for more information:
+     * https://developer.android.com/reference/android/app/Activity.html
+     */
     @Override
     protected void onPause() {
+        //calls the onPause-Method of the super class "Activity"
         super.onPause();
+
+        //unregister the GatewayListener
         StroeerProxityApi.getInstance(this).unregisterGatewayListener(this);
     }
 
+    /**
+     * This method is called whenever the sdk has to inform you about new messages
+     *
+     * @param message:              the actual message with descripion, code and data which is new
+     * @param deliverdForFirstTime: determines if this message was sent for the first time (true) or it is a copy (false). This is neccessary for reseting the app if its gone to background and was brought to the front again.
+     */
     @Override
-    public void onMessage(Message message, boolean isNew) {
-        if (isNew) {
-            this.debugView.setText(this.debugView.getText() + message.getMessage() + '\n');
+    public void onMessage(Message message, boolean deliverdForFirstTime) {
+
+        //If the message was delivered for the first time print this message to the android-log
+        if (deliverdForFirstTime) {
+            //Print the given message to the android-log
+            this.debugView.setText(this.debugView.getText() + message.getMessageType().toString() + ": " + message.getMessage() + '\n');
         }
 
+        //Check if the message is a INFO_ENTERED_BEACON_REGION
         if (message.getCode() == ResponseCode.INFO_ENTERED_BEACON_REGION) {
+            //Returns the entered beacon into the enteredBeacon variable
             Beacon enteredBeacon = (Beacon) message.getData();
+
+            //Do some logic with the enteredBeacon
         }
 
+        //Check if the message is a INFO_LEFT_BEACON_REGION
         if (message.getCode() == ResponseCode.INFO_LEFT_BEACON_REGION) {
+            //Returns the left beacon into the leftBeacon variable
             Beacon leftBeacon = (Beacon) message.getData();
+
+            //Do some logic with the leftBeacon
         }
     }
 
+    /**
+     * is called whenever a new status was gained. Look inside of Dependencies class to inform you about the possible statuses
+     *
+     * @param status:               the actual status which was gained
+     * @param deliverdForFirstTime: determines if this message was sent for the first time (true) or it is a copy (false). This is neccessary for reseting the app if its gone to background and was brought to the front again.
+     */
     @Override
     public void onStatusGained(Dependencies status, boolean deliverdForFirstTime) {
+        //Prints the given status to the android-log
         this.debugView.setText(this.debugView.getText() + status.toString() + " gained [deliverdForFirstTime: " + deliverdForFirstTime + "]\n");
     }
 
+    /**
+     * is called whenever a status was revoked. Look inside of Dependencies class to inform you about the possible statuses
+     *
+     * @param status:               the actual status which was revoked
+     * @param deliverdForFirstTime: determines if this message was sent for the first time (true) or it is a copy (false). This is neccessary for reseting the app if its gone to background and was brought to the front again.
+     */
     @Override
     public void onStatusRevoked(Dependencies status, boolean deliverdForFirstTime) {
+        //If the message was delivered for the first time print this message to the android-log
         if (deliverdForFirstTime) {
+            //Prints the given status to the android-log
             this.debugView.setText(this.debugView.getText() + status.toString() + " revoked\n");
         }
     }
--- demo/app/src/main/java/de/stroeer/demoapp/MyBroadCastReceiver.java
+++ demo/app/src/main/java/de/stroeer/demoapp/MyBroadCastReceiver.java
@@ -10,56 +10,93 @@ import de.stroeer.proxity.gateway.MessageIntent;
 import de.stroeer.proxity.gateway.StatusIntent;
 import de.stroeer.proxity.gateway.Dependencies;
 
+
 /**
- * Created by mfriedl on 16.10.2014.
+ * <p>
+ * If you like to get all information of the sdk, also if the app is in background,
+ * you have to implement a BroadcastReceiver and register it for the actions:
+ * <p>
+ * "de.stroeer.proxity.gateway.StatusIntent"
+ * "de.stroeer.proxity.gateway.MessageIntent"
+ * <p>
+ * See AndroidManifest.xml line 22-29
+ * <p>
+ * This is small example implementation of this BroadcastReceiver which prints all messages to
+ * android-log
  */
 public class MyBroadCastReceiver extends BroadcastReceiver {
+    //Default Tag of a class. Used to tag the logs for this name
     private static final String LOG_TAG = MyBroadCastReceiver.class.getSimpleName();
 
+   /*
+    * Each time the sdk sends a new message or status change this method is called.
+    *
+    * To get the needed information out of the intent you have to identify the type of intent,
+    * according to the actionname (line 48 and line 71). The Action is either MessageIntent.ACTION_NAME
+    * or StatusIntent.ACTION_NAME.
+    *
+    * A convenience method for the MessageIntent and StatusIntent are implemented (line 49 and 72)
+    *
+    * After you got the corresponding Intent (MessageIntent/StatusIntent) you can get all necessary
+    * information out of this instances. (line 52 - 68 and line 75- 77)
+    */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Receiver", this.toString());
+        //Checks if the sent intent is a MessageIntent
         if (intent.getAction().equals(MessageIntent.ACTION_NAME)) {
+            //Parse the intent to the corresponding MessageIntent to get easy access to all information
             MessageIntent messageIntent = MessageIntent.fromIntent(intent);
+            //Returns the Message into the message variable
             Message message = messageIntent.getMessage();
+            //Switch over ResponseCode to define some specific logic for each needed responseCode
+            //See JavaDoc of ResponseCode for more information about each Type
             switch (message.getCode()) {
                 case FAILURE_CONFIGURATION:
+                    //Call helper method scanStartFailedSdkNotSetup which will print some logs
                     scanStartFailedSdkNotSetup();
                     break;
                 case FAILURE_BLE:
+                    //Call helper method scanStartFailedSdkNotSetup which will print some logs
                     onBleInitializationFailed(message.getMessage());
                     break;
+                //For each ResponseCode which isn't FAILURE_CONFIGURATION or FAILURE_BLE call unknownMessage-Method
                 default:
-                    unknownMessage(intent);
+                    //Call helper method scanStartFailedSdkNotSetup which will print some logs
+                    unknownMessage(message);
             }
         }
+        //Check if the sent intent is a StatusIntent
         if (intent.getAction().equals(StatusIntent.ACTION_NAME)) {
+            //Parse the intent to the corresponding StatusIntent to get easy access to all information
             StatusIntent statusIntent = StatusIntent.fromIntent(intent);
+            //Returns the Status into the status variable
             Dependencies status = statusIntent.getStatus();
+            //Prints the given status and whether it was gained or revoked to the android-log
             addToDebugView(LOG_TAG, "Status " + status.toString() + (statusIntent.isGained() ? " gained" : " revoked"));
-            if (statusIntent.isGained()) {
-                switch (status) {
-                }
-            }
         }
     }
 
+    //you cannot use the sdk because it was not set up correctly before
     public void scanStartFailedSdkNotSetup() {
-        //you cannot use the sdk because it was not set up correctly before
+        //Prints the given message to the android-log
         addToDebugView(LOG_TAG, "scanStartFailedSdkNotSetup");
     }
 
-    public void unknownMessage(Intent intent) {
-        //this methode should not be called, as there should not be unknown messages
-        Message message = (Message) intent.getSerializableExtra("message");
+    //helper method which will print some logs
+    public void unknownMessage(Message message) {
+        //Prints the given message to the android-log
         addToDebugView(LOG_TAG, message.getMessage());
     }
 
+    //helper method which will print some logs
     public void onBleInitializationFailed(String bleError) {
+        //Prints the given bleError-string to the android-log
         addToDebugView(LOG_TAG, "onBleInitializationFailed: " + bleError);
     }
 
+    //helper method which will print some logs
     protected void addToDebugView(String logtag, String message) {
+        //Prints the given message-string to the android-log
         Log.d(logtag, message);
     }
 }
